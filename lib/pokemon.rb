3b4f0850707735bf27de4cd3c1fddd853651730e
Antonio Reid <read.musik@gmail.com>
2018-03-08 01:12:55 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -30,7 +30,7 @@ class Pokemon
     #find the object in db
     #@hp = hp
 
-    db.execute("UPDATE pokemon SET hp = new_hp WHERE pokemon.id = id;")
+    db.execute("UPDATE pokemon SET hp = new_hp WHERE id = 1;")
     #binding.pry
   end
 end
